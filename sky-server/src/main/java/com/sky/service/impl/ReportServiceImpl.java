3eba064988a88167d7045f12a1d0f853044e8bd9
song <song@email.com>
2025-09-24 20:23:08 +0800

    用户数据统计功能代码开发

--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -2,8 +2,10 @@ package com.sky.service.impl;
 
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,18 +24,22 @@ import java.util.Map;
 public class ReportServiceImpl implements ReportService {
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    private UserMapper userMapper;
+
     /**
      * 统计指定时间区间内的营业额统计数据
+     *
      * @param begin
      * @param end
      * @return
      */
     public TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end) {
         //当前集合用于存放begin到end范围内每天的日期
-        List<LocalDate> dateList=new ArrayList<>();
+        List<LocalDate> dateList = new ArrayList<>();
         dateList.add(begin);
-        while(!begin.equals(end)){
-            begin=begin.plusDays(1);
+        while (!begin.equals(end)) {
+            begin = begin.plusDays(1);
             dateList.add(begin);
         }
         List<Double> tunoverList = new ArrayList<>();
@@ -42,21 +48,61 @@ public class ReportServiceImpl implements ReportService {
             LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
             LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
             //select sum(amount) from orders where order_time>? and order_time <? and status=5
-            Map map=new HashMap<>();
-            map.put("begin",beginTime);
-            map.put("end",endTime);
+            Map map = new HashMap<>();
+            map.put("begin", beginTime);
+            map.put("end", endTime);
             map.put("status", Orders.COMPLETED);
-            Double turnover= orderMapper.sumByMap(map);
+            Double turnover = orderMapper.sumByMap(map);
             turnover = turnover == null ? 0.0 : turnover;
             tunoverList.add(turnover);
         }
         //封装返回结果
         String dateStr = StringUtils.join(dateList, ",");
-        String turnoverStr=StringUtils.join(tunoverList,",");
+        String turnoverStr = StringUtils.join(tunoverList, ",");
         return TurnoverReportVO
                 .builder()
                 .dateList(dateStr)
                 .turnoverList(turnoverStr)
+                .build();
+    }
+
+    /**
+     * 统计指定时间区间内的用户数据
+     *
+     * @param begin
+     * @param end
+     * @return
+     */
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+        //存放从begin到end之间的每天对应的日期
+        List<LocalDate> dateList = new ArrayList<>();
+        while (!begin.equals(end)) {
+            dateList.add(begin);
+            begin = begin.plusDays(1);
+        }
+        //存放每天新增用户数量 select count(id) from user where create_time < ? and create_time > ?
+        List<Integer> newUserList = new ArrayList<>();
+        //存放每天总用户数量 select count(id) from user where create_time < ?
+        List<Integer> totalUserList = new ArrayList<>();
+        for (LocalDate date : dateList) {
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+            Map map = new HashMap<>();
+            map.put("end", endTime);
+            //总用户数量
+            Integer totalUser = userMapper.countByMap(map);
+            totalUserList.add(totalUser);
+            map.put("begin", beginTime);
+            //每天总用户数量
+            Integer newUser = userMapper.countByMap(map);
+            newUserList.add(newUser);
+        }
+
+
+        return UserReportVO.builder()
+                .dateList(StringUtils.join(dateList, ","))
+                .totalUserList(StringUtils.join(totalUserList, ","))
+                .newUserList(StringUtils.join(newUserList, ","))
                 .build();
     }
 }
